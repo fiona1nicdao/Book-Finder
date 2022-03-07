2d5210ac1318c4dcdc74219de732fef1324d78f4
FionaN <fiona1941nicdao@gmail.com>
2022-03-06 23:06:46 -0600

    update typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql `
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: ID
+        image: String
+        link: String
+        title: String!
+    }
+    input InputBook {
+        authors:[String]
+        description:String!
+        title:String!
+        bookId:String!
+    }
+
+    type Query {
+        me: User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password:String! ):Auth
+        login(email: String!, password: String!): Auth
+        saveBook(newbook: InputBook!):User
+        removeBook(bookId: ID!):User
+    }
+`
+module.exports = typeDefs;
+
